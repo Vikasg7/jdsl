9c77e58cab949873d3ad184c0d4d57a2277d81ff
Vikas <v33vikasgautam@gmail.com>
2023-06-23 18:33:20 +0530

    Updated doc-string for combinator-flip

--- src/jdsl/combinator_flip.clj
+++ src/jdsl/combinator_flip.clj
@@ -1,8 +1,8 @@
 (ns jdsl.combinator-flip
-  "This namespace creates and holds version of jdsl.combinator functions with parameters
-   in reverse orders. This might come in handy while extending parses with -> and ->>
-   (thread macros). So when you require the jdsl.flipped namespace, you should be able
-   to access functions in jsdl.combinators with their arguments flipped."
+  "This namespace creates and holds version of jdsl.combinator functions (marked <Flipable> in doc-string) 
+   with parameters in reverse orders. This might come in handy while extending parses with -> and ->> 
+   (thread macros). So when you require the jdsl.combinator-flip namespace, you should be able to access functions 
+   in jsdl.combinators (marked <Flipable> in doc-string) with their arguments flipped i.e. in reverse order."
   (:refer-clojure :exclude [peek map apply])
   (:require [clojure.string :as str]))
 
@@ -18,7 +18,7 @@
 (defn- copy-doc-string
   "Copies over doc string from other #'var"
   [from-var to-var]
-  (let [new-doc (str "`NOTE: Version with flipped parameters.`  " \newline
+  (let [new-doc (str "**NOTE**: *Read following docs with flipped parameters in mind.*  " \newline
                      (:doc (meta from-var)))]
   (alter-meta! to-var update-in [:doc] (constantly new-doc))))
 
